c676c2706489c4d434138bcf4bc4f59c7fa7c4ac
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-09 14:40:09 +0900

    Fix travis error

---
+++ test/index_test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const index = require('../lib')
+const assert = require('assert')
+
+describe('index', () => {
+  it('Eval index', () => {
+    assert.ok(index)
+  })
+})
+
+/* global describe, it */
