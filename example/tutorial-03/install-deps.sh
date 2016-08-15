83904173e4ccd0c378401cee335275167f8ec86a
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-15 20:56:52 +0900

    Fix links

--- example/tutorial-03/install-deps.sh
+++ example/tutorial-03/install-deps.sh
@@ -1,2 +1,4 @@
+# Install dependencies
 npm install -S sugo-actor sugo-caller sugo-hub co asleep react react-dom babel-polyfill
+# Install dev dependencies
 npm install -D browserify babelify babel-preset-es2015 babel-preset-react
